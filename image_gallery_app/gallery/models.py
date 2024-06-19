1bd5f37e1934f02b5467b969656e87c8e739677b
Michael Tao <michaeltao0713@gmail.com>
2024-06-19 17:53:03 -0400

    set up image model

--- image_gallery_app/gallery/models.py
+++ image_gallery_app/gallery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+# Images Model - Stores file location and metadata
+class Images(models.Model):
+    # Title of the image
+    title = models.CharField(max_length=100)
+    # File path to the image in storage
+    file_path = models.CharField(max_length=256)
+    # Time image was uploaded to gallery
+    upload_time = models.DateTimeField(auto_now_add=True)
+
+    # Return title of image
+    def __str__(self):
+        return self.title
