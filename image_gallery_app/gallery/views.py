60b64d28117701a7b5f985e7a334fa1962ba56c0
Michael Tao <michaeltao0713@gmail.com>
2024-06-23 13:48:14 -0400

    I have no idea what I'm doing, first page works

--- image_gallery_app/gallery/views.py
+++ image_gallery_app/gallery/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
+from .models import Images
 
-# Create your views here.
+
+# def members(request):
+#     return HttpResponse("Hello World!")
+
+def image_list(request):
+    images = Images.objects.all()
+    return render(request, 'image_list.html', {'images': images})
